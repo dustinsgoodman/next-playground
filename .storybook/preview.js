82af76dfec40ee638b2432bf019fadaa355157bc
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 14:06:14 -0600

    configure storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,27 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { addDecorator, addParameters } from '@storybook/react';
+import {
+  INITIAL_VIEWPORTS,
+} from '@storybook/addon-viewport';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+import GlobalStyle from 'shared/global-style';
+import { darkTheme } from 'components/styles/theme';
+
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+    },
+  },
+});
+
+addDecorator(Story => (
+  <ThemeProvider theme={darkTheme}>
+    <GlobalStyle />
+    <Story />
+  </ThemeProvider>
+));
+
+export const parameters = {};
