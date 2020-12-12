4afcab4bd4408d5b64db4070025710dba2f39ad1
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 13:49:14 -0600

    stub out a hello world render exmaple to verify changes

---
+++ src/components/atoms/hello-world/hello-world.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Main, H1, Description } from './hello-world.styles';
+
+const HelloWorld = () => (
+  <Container>
+    <Main>
+      <H1>Hello World</H1>
+      <Description>Lorem ipsum dolor...</Description>
+    </Main>
+  </Container>
+);
+
+export default HelloWorld;
