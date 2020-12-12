5c003b70bae78be206bc61f8a1010e62ed96c8df
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 13:48:54 -0600

    add helmet + theme provider to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
-import '../styles/globals.css';
+import { Helmet } from 'react-helmet';
+import { ThemeProvider } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import GlobalStyle from 'shared/global-style';
+import { darkTheme } from 'components/styles/theme';
 
-export default MyApp;
+const defaultTitle = 'Next Playground';
+
+const AppWrapper = ({ children }) => (
+  <ThemeProvider theme={darkTheme}>
+    <GlobalStyle />
+    <Helmet
+      htmlAttributes={{ lang: 'en' }}
+      title={defaultTitle}
+      link={[
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+      ]}
+      meta={[
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        { property: 'og:title', content: defaultTitle },
+        {
+          name: 'description',
+          content: 'A place to experiment with Next.js features.',
+        },
+      ]}
+    />
+    {children}
+  </ThemeProvider>
+);
+
+const App = ({ Component, pageProps }) => (
+  <AppWrapper>
+    <Component {...pageProps} />
+  </AppWrapper>
+);
+
+export default App;
