b161dc61530106b6d6e66f54ac058b3d9acc1021
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 18:46:49 -0600

    add more theme info

---
+++ src/components/styles/colors.js
@@ -0,0 +1,10 @@
+const ColorPallete = {
+  black: 'rgb(18, 18, 18)',
+  darkGray: 'rgb(31, 31, 31)',
+  gray: 'rgb(33, 37, 44)',
+  white: 'rgb(201, 209, 217)',
+  blue: 'rgb(137, 171, 206)',
+  blueGray: 'rgb(185, 190, 198)',
+};
+
+export default ColorPallete;
--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -1,4 +1,6 @@
+import { darken } from 'polished';
 import { BASE_SPACING, DOUBLE_BASE_SPACING } from './spacing';
+import ColorPallete from './colors';
 
 export const lightTheme = {
   spacing: {
@@ -11,5 +13,23 @@ export const darkTheme = {
   spacing: {
     BASE_SPACING,
     DOUBLE_BASE_SPACING,
+  },
+  palette: {
+    background: {
+      primary: ColorPallete.black,
+      secondary: ColorPallete.darkGray,
+    },
+    text: {
+      primary: ColorPallete.white,
+      link: ColorPallete.blue,
+    },
+    input: {
+      text: ColorPallete.blueGray,
+    },
+    info: {
+      background: ColorPallete.gray,
+      text: ColorPallete.white,
+      active: darken('0.1', ColorPallete.gray),
+    },
   },
 };
