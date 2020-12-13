d0bfb32859f8390db2e5a01239bce137d5f8538f
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 23:44:00 -0600

    remove style provider from stack

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from 'styled-components';
-import { StylesProvider } from '@material-ui/core/styles';
 
 import useApollo from 'shared/apollo';
 import GlobalStyle from 'shared/global-style';
@@ -11,33 +10,31 @@ import { darkTheme } from 'components/styles/theme';
 const defaultTitle = 'Next Playground';
 
 export const AppWrapper = ({ children }) => (
-  <StylesProvider>
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      <Helmet
-        htmlAttributes={{ lang: 'en' }}
-        title={defaultTitle}
-        link={[
-          {
-            rel: 'icon',
-            href: '/favicon.ico',
-          },
-        ]}
-        meta={[
-          {
-            name: 'viewport',
-            content: 'width=device-width, initial-scale=1',
-          },
-          { property: 'og:title', content: defaultTitle },
-          {
-            name: 'description',
-            content: 'A place to experiment with Next.js features.',
-          },
-        ]}
-      />
-      {children}
-    </ThemeProvider>
-  </StylesProvider>
+  <ThemeProvider theme={darkTheme}>
+    <GlobalStyle />
+    <Helmet
+      htmlAttributes={{ lang: 'en' }}
+      title={defaultTitle}
+      link={[
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+      ]}
+      meta={[
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        { property: 'og:title', content: defaultTitle },
+        {
+          name: 'description',
+          content: 'A place to experiment with Next.js features.',
+        },
+      ]}
+    />
+    {children}
+  </ThemeProvider>
 );
 
 const App = ({ Component, pageProps }) => {
