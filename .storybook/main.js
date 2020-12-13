6818a38bfba6caa8afd8e327d62d32b8eb3b1a05
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 19:53:19 -0600

    add storybook addon controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.jsx"
+  stories: [
+    '../src/**/*.stories.jsx'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@storybook/addon-controls',
     'storybook-addon-apollo-client'
   ]
-}
+};
