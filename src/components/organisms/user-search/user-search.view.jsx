415515e926479fc1f9be74afafa9d2f9eb92eef0
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-12-12 23:44:42 -0600

    get results rendering with pagination

--- src/components/organisms/user-search/user-search.view.jsx
+++ src/components/organisms/user-search/user-search.view.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import Loading from 'components/atoms/loading';
+import Error from 'components/atoms/error';
 import UserResult from 'components/molecules/user-result';
 import OrganizationResult from 'components/molecules/organization-result';
 
-const Loading = () => <p>loading...</p>;
-const Error = ({ error }) => <p>{error.message}</p>;
+import {
+  UserSearchContainer,
+  FormContainer,
+  FormErrorContainer,
+} from './user-search.styles';
 
 const UserSearch = ({
   results,
@@ -16,30 +23,57 @@ const UserSearch = ({
   loading,
   error,
 }) => {
-  if (loading) {
-    return <p>loading...</p>;
-  }
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = ({ searchUsers }) => setSearchQuery(searchUsers);
+
   return (
-    <>
-      <input type="text" placeholder="search field" />
+    <UserSearchContainer>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormContainer>
+          <input
+            type="text"
+            placeholder="search field"
+            name="searchUsers"
+            defaultValue={null}
+            ref={register({ required: true })}
+          />
+
+          <input type="submit" />
+        </FormContainer>
+
+        {errors.searchUsers && (
+          <FormErrorContainer>Must provide a search string</FormErrorContainer>
+        )}
+      </form>
+
       <h3>Results: {totalCount}</h3>
-      <button>prev page</button>
-      <button>next page</button>
+      {hasPrevPage && (
+        <button type="button" onClick={gotoPrevPage}>
+          Previous
+        </button>
+      )}
+      {hasNextPage && (
+        <button type="button" onClick={gotoNextPage}>
+          Next
+        </button>
+      )}
       {loading && <Loading />}
       {error && <Error error={error} />}
-      {results.map((node) => {
-        // eslint-disable-next-line no-underscore-dangle
-        switch (node.__typename) {
-          case 'User':
-            return <UserResult key={node.login} {...node} />;
-          case 'Organization':
-            return <OrganizationResult key={node.login} {...node} />;
-          default:
-            console.warn('Cannot render this type of result');
-            return null;
-        }
-      })}
-    </>
+      {!loading &&
+        !error &&
+        results?.map((node) => {
+          // eslint-disable-next-line no-underscore-dangle
+          switch (node.__typename) {
+            case 'User':
+              return <UserResult key={node.login} {...node} />;
+            case 'Organization':
+              return <OrganizationResult key={node.login} {...node} />;
+            default:
+              console.warn('Cannot render this type of result');
+              return null;
+          }
+        })}
+    </UserSearchContainer>
   );
 };
 
